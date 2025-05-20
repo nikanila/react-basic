64afe48c4ace29818c7f7a155e5ba85d707ec954
Hona <nguyenmanhhoan2k4@gmail.com>
2025-05-20 21:40:29 +0700

    write the first state

--- src/views/Example/MyComponent.js
+++ src/views/Example/MyComponent.js
@@ -2,9 +2,31 @@ import React from "react";
 
 class MyComponent extends React.Component {
 
+    state = {
+        name: 'Hoan',
+        major: 'Information Technology'
+    }
+
+    handleOnChangeName = (event) => {
+        this.setState({
+            name: event.target.value
+        })
+    }
+
     render() {
         return (
-            <div>hello my component</div>
+            <>
+                <div className="first">
+                    <input value={this.state.name} type="text" 
+                    onChange={(event) => this.handleOnChangeName(event)}></input>
+                </div>
+                <div className="second">
+                    hello my component, my name is {this.state.name}
+                </div>
+                <div className='third'>
+                    my major is {this.state.major}
+                </div>
+            </>
         )
     }
 }
