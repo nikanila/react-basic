3a50de9fc498ff2e8096f587f8ede4a2a169dc40
Hona <nguyenmanhhoan2k4@gmail.com>
2025-05-20 21:08:52 +0700

    first example of component

---
+++ src/views/Example/MyComponent.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+class MyComponent extends React.Component {
+
+    render() {
+        return (
+            <div>hello my component</div>
+        )
+    }
+}
+
+export default MyComponent;
